c45ca4c47149333760a8aa20fbc4b4d65abbe063
kayxean <36327242+kayxean@users.noreply.github.com>
2026-01-15 22:44:28 +0700

    feat: add css format and parser

--- src/core/format.ts
+++ src/core/format.ts
@@ -2,24 +2,21 @@ import type { ColorMode, ColorSpace } from './types';
 
 const format = (num: number, precision = 2): string => {
   if (num % 1 === 0) return num.toString();
-
   const factor = precision === 4 ? 10000 : 100;
-
   return (Math.round(num * factor) / factor).toString();
 };
 
 export const formatCss = (
   mode: ColorMode,
   values: ColorSpace<ColorMode>,
-  percent?: boolean,
-  angle?: boolean,
+  alpha?: number,
 ): string => {
-  const v1 = values[0];
-  const v2 = values[1];
-  const v3 = values[2];
+  const [v1, v2, v3] = values;
+
+  const a = alpha !== undefined && alpha < 1 ? ` / ${format(alpha)}` : '';
 
   if (mode === 'rgb') {
-    return `rgb(${Math.round(v1 * 255)} ${Math.round(v2 * 255)} ${Math.round(v3 * 255)})`;
+    return `rgb(${Math.round(v1 * 255)} ${Math.round(v2 * 255)} ${Math.round(v3 * 255)}${a})`;
   }
 
   let c1: string;
@@ -27,20 +24,20 @@ export const formatCss = (
   let c3: string;
 
   if (mode === 'hsl' || mode === 'hwb') {
-    c1 = angle ? `${format(v1)}deg` : format(v1);
-    c2 = percent ? `${format(v2 * 100)}%` : format(v2 * 100);
-    c3 = percent ? `${format(v3 * 100)}%` : format(v3 * 100);
+    c1 = `${format(v1)}deg`;
+    c2 = `${format(v2 * 100)}%`;
+    c3 = `${format(v3 * 100)}%`;
   } else if (mode === 'lab' || mode === 'lch') {
-    c1 = percent ? `${format(v1)}%` : format(v1);
+    c1 = `${format(v1)}%`;
     c2 = format(v2);
-    c3 = mode === 'lch' && angle ? `${format(v3)}deg` : format(v3);
+    c3 = mode === 'lch' ? `${format(v3)}deg` : format(v3);
   } else if (mode === 'oklab' || mode === 'oklch') {
-    c1 = percent ? `${format(v1 * 100)}%` : format(v1 * 100);
+    c1 = `${format(v1 * 100)}%`;
     c2 = format(v2, 4);
-    c3 = mode === 'oklch' && angle ? `${format(v3)}deg` : format(v3, 4);
+    c3 = mode === 'oklch' ? `${format(v3)}deg` : format(v3, 4);
   } else {
     throw new Error(`Unsupported mode: ${mode}`);
   }
 
-  return `${mode}(${c1} ${c2} ${c3})`;
+  return `${mode}(${c1} ${c2} ${c3}${a})`;
 };
